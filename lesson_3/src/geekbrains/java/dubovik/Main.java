a1e9a0426ae7deb3c77037d7d2a093cad8ebe6e7
dubandro <dubandro@users.noreply.github.com>
2020-12-31 19:57:13 +0200

    Update Main.java

--- lesson_3/src/geekbrains/java/dubovik/Main.java
+++ lesson_3/src/geekbrains/java/dubovik/Main.java
@@ -10,8 +10,8 @@ public class Main {
         System.out.println("Задание 1");
         guess_number();
 
-        /*System.out.println("\nЗадание 2");
-        guess_words(); допишу завтра между салатами))*/
+        System.out.println("\nЗадание 2");
+        guess_words();
 
     }
 
@@ -39,12 +39,55 @@ public class Main {
                 }
             }
             if (try_num > 3) System.out.println("Вы не угадали :(");
-            
-            System.out.print("Если хотите сыграть ещё, введите 1, если хотите выйти - 0: ");
-            input_num = input.nextInt();
+            do {
+                System.out.print("Если хотите сыграть ещё, введите 1, если хотите выйти - 0: ");
+                input_num = input.nextInt();
+            }
+            while (input_num < 0 || input_num > 1);
+
             if (input_num == 1) continue;
             if (input_num == 0) break;
         }
+        //input.close();
+    }
+
+    public static void guess_words() {
+        Scanner input = new Scanner(System.in);
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli",
+                "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom",
+                "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        Random ran = new Random();
+        String rand_words = words[ran.nextInt(words.length)];
+        System.out.println("(" + rand_words + ")");
+        System.out.print("Угадайте загаданное слово: ");
+        while (true) {
+            String input_words = input.next();
+            if (rand_words.equals(input_words)) {
+                System.out.println("Вы угадали, это слово - " + input_words);
+                break;
+            }
+            else {
+                System.out.print("Вы не угадали, но в загаданном и введённом слове могут совпадать каие-то буквы,\n" +
+                        "секундочку проверяем: ");
+                char[] randWord = rand_words.toCharArray();
+                char[] inpuWord = input_words.toCharArray();
+                for (int i = 0; i < 15 ; i++) {
+                    if (i > randWord.length-1 || i > inpuWord.length-1) {
+                        System.out.print("#");
+                    }
+                    else {
+                        if (randWord[i] == inpuWord[i]) {
+                            System.out.print(randWord[i]);
+                        }
+                        else {
+                            System.out.print("#");
+                        }
+                    }
+                }
+                System.out.print("\nПопробуйте ещё раз: ");
+            }
+        }
         input.close();
     }
+
 }
