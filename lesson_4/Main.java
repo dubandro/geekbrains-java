14ceac1d8ef00bb9123869011f19027403df7730
dubandro <dubandro@users.noreply.github.com>
2021-01-05 17:48:56 +0200

    Create Main.java

---
+++ lesson_4/Main.java
@@ -0,0 +1,8 @@
+package geekbrains.java.dubovik;
+// Домашнее задание к 4му уроку, в связи с болезнью прошу отсрочку (писал через форму обращения) 2-3 дня
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
